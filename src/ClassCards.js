d3c3c9952b5504820f0c7281349f1e629017fe71
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-04-18 16:36:31 -0700

    Cards added, buttons not styling for some reason

--- src/ClassCards.js
+++ src/ClassCards.js
@@ -1,9 +1,82 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 function ClassCards() {
     return (
         <div>
-            <h1>Class Cards Go Here</h1>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Barbarian</Card.Title>
+                    <Button variant='primary'>Choose Barbarian</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Bard</Card.Title>
+                    <Button variant='info'>Choose Bard</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Cleric</Card.Title>
+                    <Button variant='info'>Choose Cleric</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Druid</Card.Title>
+                    <Button variant='info'>Choose Druid</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Fighter</Card.Title>
+                    <Button variant='primary'>Choose Fighter</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Monk</Card.Title>
+                    <Button variant='primary'>Choose Monk</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Paladin</Card.Title>
+                    <Button variant='primary'>Choose Paladin</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Ranger</Card.Title>
+                    <Button variant='primary'>Choose Ranger</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Rogue</Card.Title>
+                    <Button variant='primary'>Choose Rogue</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Sorcerer</Card.Title>
+                    <Button variant='info'>Choose Sorcerer</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Warlock</Card.Title>
+                    <Button variant='info'>Choose Warlock</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Wizard</Card.Title>
+                    <Button variant='info'>Choose Wizard</Button>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
--- src/RaceCards.js
+++ src/RaceCards.js
@@ -1,9 +1,70 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 function RaceCards() {
     return (
         <div>
-            <h1>Race Cards Go Here</h1>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Dwarf</Card.Title>
+                    <Button variant='danger'>Choose Dwarf</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Elf</Card.Title>
+                    <Button variant='success'>Choose Elf</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Halfling</Card.Title>
+                    <Button variant='warning'>Choose Halfling</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Human</Card.Title>
+                    <Button variant='secondary'>Choose Human</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Dragonborn</Card.Title>
+                    <Button variant='danger'>Choose Dragonborn</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Gnome</Card.Title>
+                    <Button variant='success'>Choose Gnome</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Half-Elf</Card.Title>
+                    <Button variant='success'>Choose Half-Elf</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Half-Orc</Card.Title>
+                    <Button variant='secondary'>Choose Half-Orc</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Tiefling</Card.Title>
+                    <Button variant='dark'>Choose Tiefling</Button>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Aasimar</Card.Title>
+                    <Button variant='light'>Choose Aasimar</Button>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
